0ddb5f0f850d2c4cd7980d13b415732e750a8e79
Rahul Singh <rscodedocs@gmail.com>
2020-06-18 12:34:32 +0530

    success page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -8,7 +8,16 @@ const NewsLetter = () => {
         <div className="page-center">
           <h2>Get all latest stories to your inbox</h2>
           <h4>I write to my friends every few weeks</h4>
-          <form className="contact-form">
+          <form
+            className="contact-form"
+            name="contact"
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            action="/sucess"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <input
               type="text"
               className="form-control"
